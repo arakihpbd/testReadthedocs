240dc1dd7d6ed9f05eb487fa6fa1b218f0a09fd8
arakihpbd <akira.univinf@gmail.com>
2021-11-16 11:24:54 +0900

    changEN

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'ja'
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -64,3 +64,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+locale_dirs = ['locale/']
+gettext_compact = False
